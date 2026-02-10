9372140c64f7a56535bf3a94addf4b7d3b629496
Petron84 <phatn5479@gmail.com>
2026-02-10 16:59:26 -0600

    improved readPic function

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import numpy as np
 #sys.path.append("/main-0203")
 #from geminicode import geminihelper
 
-def readMyPic():
-    img = cv.imread(".venv\\main-0203\\media\\blanktreble.png") # Modified to accommodate the way my files are organized and the virtual environment system
+def readPic():
+    img = cv.imread("media\\blanktreble.png") # Modified to accommodate the way my files are organized and the virtual environment system
     cv.waitKey(0)
     
     # img.shape returns a tuple: (Height, Width, Channels)
@@ -45,6 +45,42 @@ def readMyPic():
     # Wait for a key press before closing the window
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+
+#Phat's code here
+def readPic2():
+    img = cv.imread("media\\blanktreble.png") # Using your uploaded file name
+    if img is None:
+        print("Image not found")
+        return
+
+    height, width, _ = img.shape
+    instaff = False
+    
+    # We only need to check the left side of the image where the clef is
+    # Checking the first 20% of the width is usually enough
+    clef_zone = int(width * 0.2)
+
+    for i in range(1, height - 1):
+        # Find the darkest pixel in the current row (within the clef zone)
+        # 0 is black, 255 is white.
+        min_val = np.min(img[i, 0:clef_zone])
+
+        # If we find a dark pixel (< 128) and we aren't "in staff" yet
+        if not instaff and min_val < 128:
+            instaff = True
+            # Draw blue line at the very top of the clef
+            img[i, :] = [255, 0, 0] 
+
+        # If the entire row is white (> 250) and we were "in staff"
+        elif instaff and np.min(img[i, 0:clef_zone]) > 250:
+            instaff = False
+            # Draw green line at the very bottom of the clef
+            img[i-1, :] = [0, 255, 0]
+
+    cv.imshow("Detected Boundaries", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
     
  
  
@@ -54,6 +90,6 @@ def findFirstBlack(image,segment):
     
     
 if __name__ == "__main__":
-    readMyPic()
+    readPic2()
 
     #geminihelper()
