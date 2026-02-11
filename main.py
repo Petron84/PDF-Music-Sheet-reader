0c607c470cc2b48f3154ebc6b919549cd73a0015
Petron84 <phatn5479@gmail.com>
2026-02-10 20:16:43 -0600

    dynamic staff detection test

--- main.py
+++ main.py
@@ -2,94 +2,109 @@ import cv2 as cv
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-#import sys
-#sys.path.append("/main-0203")
 #from geminicode import geminihelper
 
 def readPic():
-    img = cv.imread("media\\blanktreble.png") # Modified to accommodate the way my files are organized and the virtual environment system
-    cv.waitKey(0)
-    
-    # img.shape returns a tuple: (Height, Width, Channels)
-    height, width, channels = img.shape
-    print(img.shape)
-    #cv.imshow("Displayed Image", img)
-    #segmentsOfFive = width/5
-
-    #listDepths = []
-    #for i in range(0,width,int(segmentsOfFive)):
-        #listDepths.append(findFirstBlack(img, i))
-    
-    instaff = False
-    for i in range(1,height,3):
-        curRowSet = [0,0,0]
-        for j in range (0,width):
-            for z in range(-1,2):
-                curRowSet[0] += int(img[i+z,j][0])
-                curRowSet[1] += int(img[i+z,j][1])
-                curRowSet[2] += int(img[i+z,j][2])
-        for rowSetIn in range(0,3):
-            curRowSet[rowSetIn] = curRowSet[rowSetIn] / (width*3)
-        if (not instaff) and (sum(curRowSet)/len(curRowSet) < 192):
-            instaff = True
-            for j in range (0,width):
-                img[i,j] = [255,0,0]
-        # else:
-            # print(sum(curRowSet)/len(curRowSet))
-        if (instaff) and (sum(curRowSet)/len(curRowSet) > 254):
-            instaff = False
-            for j in range (0,width):
-                img[i-3,j] = [0,255,0]
-    cv.imshow("Displayed Image", img)
-
-    # Wait for a key press before closing the window
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-
-
-#Phat's code here
-def readPic2():
-    img = cv.imread("media\\blanktreble.png") # Using your uploaded file name
+    img = cv.imread("media\\twinkle star.png") 
     if img is None:
         print("Image not found")
         return
 
-    height, width, _ = img.shape
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    _, binary = cv.threshold(gray, 200, 255, cv.THRESH_BINARY_INV)
+
+    height, width = binary.shape
     instaff = False
     
-    # We only need to check the left side of the image where the clef is
-    # Checking the first 20% of the width is usually enough
-    clef_zone = int(width * 0.2)
+    # Store detected staff boundaries as (top, bottom) tuples
+    potential_staves = []
+    current_start = 0
 
-    for i in range(1, height - 1):
-        # Find the darkest pixel in the current row (within the clef zone)
-        # 0 is black, 255 is white.
-        min_val = np.min(img[i, 0:clef_zone])
+    # Search zone for the clef
+    start_x = int(width * 0.05)
+    end_x = int(width * 0.15)
 
-        # If we find a dark pixel (< 128) and we aren't "in staff" yet
-        if not instaff and min_val < 128:
+    for i in range(height):
+        black_pixel_count = np.sum(binary[i, start_x:end_x]) / 255
+
+        if not instaff and black_pixel_count > 10:
             instaff = True
-            # Draw blue line at the very top of the clef
-            img[i, :] = [255, 0, 0] 
+            current_start = i 
 
-        # If the entire row is white (> 250) and we were "in staff"
-        elif instaff and np.min(img[i, 0:clef_zone]) > 250:
+        elif instaff and black_pixel_count < 2:
             instaff = False
-            # Draw green line at the very bottom of the clef
-            img[i-1, :] = [0, 255, 0]
+            current_end = i
+            # ADDED: Only keep segments that are tall enough (e.g., > 60 pixels)
+            if (current_end - current_start) > 30:
+                potential_staves.append((current_start, current_end))
 
-    cv.imshow("Detected Boundaries", img)
+    # Now draw the lines only for the valid staves
+    for (top, bottom) in potential_staves:
+        # Draw blue line at the top
+        img[top, :] = [255, 0, 0]
+        # Draw green line at the bottom
+        img[bottom, :] = [0, 255, 0]
+        
+        print(f"Detected staff at Y-range: {top} to {bottom} (Height: {bottom-top})")
+
+    cv.imshow("Cleaned Staff Detection", img)
     cv.waitKey(0)
     cv.destroyAllWindows()
-    
- 
+
+
+def dynamic_readPic():
+    # this is a dynamic version of readPic, with the value of "tallness" be determined
+    # by the height of the first detected staff. This is to accomodate for different sizes of sheet music.
+    img = cv.imread("media\\twinkle star.png")
+    if img is None:
+        print("Image not found")
+        return
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    _, binary = cv.threshold(gray, 200, 255, cv.THRESH_BINARY_INV)
+    height, width = binary.shape
+    instaff = False
+
+    potential_staves = []
+    current_start = 0
+    start_x = int(width * 0.05)
+    end_x = int(width * 0.15)
+
+    first_staff_height = None
+    for i in range(height):
+        black_pixel_count = np.sum(binary[i, start_x:end_x]) / 255
+
+        if not instaff and black_pixel_count > 10:
+            instaff = True
+            current_start = i 
+
+        # allow some noise, if there are too many white pixels, we consider it as the end of the staff
+        # this value of 0.75 is just a guess value. aka a ratio of black pixels to total pixels in the search zone.
+        # adjustments may be needed based on the quality of the image and the size of the search zone.
+        elif instaff and black_pixel_count <= 0.75: 
+            instaff = False
+            current_end = i
+            staff_height = current_end - current_start
+            
+            if first_staff_height is None:
+                first_staff_height = staff_height
+            
+            # Use the first detected staff height as a threshold
+            if staff_height > (first_staff_height * 0.5):  # Allow some variation
+                potential_staves.append((current_start, current_end))
+    for (top, bottom) in potential_staves:
+        img[top, :] = [255, 0, 0]
+        img[bottom, :] = [0, 255, 0]
+        print(f"Detected staff at Y-range: {top} to {bottom} (Height: {bottom-top})")
+    cv.imshow("Dynamic Staff Detection", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
  
 def findFirstBlack(image,segment):
-     print("Hello World")
+     pass
         
     
     
 if __name__ == "__main__":
-    readPic2()
+    dynamic_readPic()
 
     #geminihelper()
