3f8d27b5535fb56b645725a7bacf47184a1617a0
Betul Zora <zorabtl@gmail.com>
2026-04-28 18:13:23 -0500

    pushing to make sure the team has pictures of take pictures

--- datamigration.py
+++ datamigration.py
@@ -52,7 +52,30 @@ def check_Otherway(target_dir):
 def main():
     picture_dir = 'media\\actionmodeldataset_v2\\'
     target_dir = 'media\\aside\\'
-    double_check_pictures(target_dir)
-    check_Otherway(target_dir)
+    # double_check_pictures(target_dir)
+    # check_Otherway(target_dir)
+    move_all_garbage(picture_dir, target_dir)
+
+def move_all_garbage(picture_dir, target_dir):
+    """We will iterate through the file names in actionlog.txt,
+    we will comma separate each line to title and label,
+    when the label is 0 we will open that picture in picture_dir using cv
+    then we will save that same picture into target_dir, keeping the same name."""
+    with open('actionlog.txt', 'r') as f:
+        for line in f:
+            title, label = line.strip().split(',')
+            print(label)
+            if int(label) == 0:
+                print(title, label)
+                img_path = os.path.join(picture_dir, title)
+                img = cv.imread(img_path, cv.IMREAD_GRAYSCALE) 
+                if img is not None:
+                    target_path = os.path.join(target_dir, title)
+                    cv.imwrite(target_path, img)
+                    with open('garbagelog.txt', 'a') as log_file:
+                        log_file.write(f"{title},{label}\n")
+                else:
+                    print(f"Warning: Could not read image {img_path}")
+
 
 main()
