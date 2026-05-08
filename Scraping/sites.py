b42a6f4bb8da051346451570b7796cef781c4f53
Wyatt Ellingson <2019wellingson@gmail.com>
2026-05-07 20:08:04 -0500

    Pushing scraper into branch

---
+++ Scraping/sites.py
@@ -0,0 +1,21 @@
+sites = [
+    {
+        "name" : "Mutopia",
+        "url": "https://www.mutopiaproject.org/index.html",
+        "file_type": ".pdf",
+        "keyword": None
+
+    },
+    {
+        "name": "IMSLP",
+        "url": "https://imslp.org/wiki/",
+        "file_type": ".pdf",
+        "keyword": None
+    },
+    {
+        "name": "PianoStreet",
+        "url": "https://www.pianostreet.com/piano_music/download_1/sheet_1.php",
+        "file_type": ".pdf",
+        "keyword": None
+    }
+    ]
