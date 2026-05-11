180d62c359d2ae51d47685ecdcd91f68067245eb
Betul Zora <zorabtl@gmail.com>
2026-05-11 17:58:52 -0500

    fixed the main

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def findFirstBlack(image,segment):
 if __name__ == "__main__":
     install()
     fetchsheet()
-    imagepath = "media\\twinkletwinklelittlestar.png"
+    imagepath = "media\\selected_sheet.png"
     lineContour = LineContour(imagepath)
     
 
